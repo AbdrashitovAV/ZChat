3a90e74452bb2e1c4349e3871d7dfc4f0dec1138
abdrashitovav <abdrashitovav@gmail.com>
2016-11-24 23:38:48 +0700

    Fix for assembly naming in assembly info

--- ZChat.Client.Communcation/Properties/AssemblyInfo.cs
+++ ZChat.Client.Communcation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WCFChat.Client.Communcation")]
+[assembly: AssemblyTitle("ZChat.Client.Communcation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WCFChat.Client.Communcation")]
+[assembly: AssemblyProduct("ZChat.Client.Communcation")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- ZChat.Shared/Properties/AssemblyInfo.cs
+++ ZChat.Shared/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WCFChatContracts")]
+[assembly: AssemblyTitle("ZChat.Shared")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WCFChatContracts")]
+[assembly: AssemblyProduct("ZChat.Shared")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
